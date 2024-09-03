f3987fedd7e04bb289daf0cc998860edba470751
rockyhuiop <rockyhuiop@gmail.com>
2024-09-03 14:13:14 +0800

    not finished

--- Assets/IRONHEAD Games/Scripts/VR Avatar Scripts/AvatarInputConverter.cs
+++ Assets/IRONHEAD Games/Scripts/VR Avatar Scripts/AvatarInputConverter.cs
@@ -12,6 +12,7 @@ public class AvatarInputConverter : MonoBehaviour
 
     public Transform AvatarHand_Left;
     public Transform AvatarHand_Right;
+    public Transform XRRig;
 
     //XRRig Transforms
     public Transform XRHead;
@@ -21,15 +22,44 @@ public class AvatarInputConverter : MonoBehaviour
 
     public Vector3 headPositionOffset;
     public Vector3 handRotationOffset;
+    public Vector3 previousXRHead;
+    public Vector3 _cameraStartingPosition;
+    private bool _waitingForCameraMovement = true;
+    public CharacterController characterController;
+    public bool IsGrounded;
+    public LayerMask GroundedLayerMask;
+    public float GroundedDistance = .02f;
+    public Vector3 GroundNormal { get; set; }
 
-    
+    private void Awake()
+    {
+        _cameraStartingPosition = XRHead.localPosition;
+        characterController = GetComponent<CharacterController>();
+    }
 
     // Update is called once per frame
     void Update()
     {
+        //previousXRHead = MainAvatarTransform.position;
+
         //Head and Body synch
+        var delta = XRHead.transform.position - XRRig.transform.position;
+        delta.y = 0f;
+        if (delta.magnitude > 0.1f)
+        {
+            delta = Vector3.ProjectOnPlane(delta, GroundNormal);
+            XRRig.position += delta;
+            XRHead.position -= delta;
+        }
+        CheckGrounded();
         MainAvatarTransform.position = Vector3.Lerp(MainAvatarTransform.position, XRHead.position + headPositionOffset, 0.5f);
+        //MainAvatarTransform.position = XRHead.position + headPositionOffset;
         AvatarHead.rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.rotation, 0.5f);
+        //var delta = MainAvatarTransform.position - previousXRHead;
+        //XRRig.position += delta;
+
+
+
         if (AvatarBody!=null)
         {
             AvatarBody.rotation = Quaternion.Lerp(AvatarBody.rotation, Quaternion.Euler(new Vector3(0, AvatarHead.rotation.eulerAngles.y, 0)), 0.05f);
@@ -41,5 +71,51 @@ public class AvatarInputConverter : MonoBehaviour
 
         AvatarHand_Left.position = Vector3.Lerp(AvatarHand_Left.position,XRHand_Left.position,0.5f);
         AvatarHand_Left.rotation = Quaternion.Lerp(AvatarHand_Left.rotation,XRHand_Left.rotation,0.5f)*Quaternion.Euler(handRotationOffset);
+    }
+
+
+    protected virtual void CheckCameraMovement()
+    {
+        if (Vector3.Distance(_cameraStartingPosition, XRHead.transform.localPosition) < .05f)
+        {
+            return;
+        }
+
+        var delta = XRHead.transform.position - XRRig.transform.position;
+        delta.y = 0f;
+        XRHead.transform.position -= delta;
+        _waitingForCameraMovement = false;
+        previousXRHead = transform.position;
+    }
+    protected virtual void CheckGrounded()
+    {
+        var radius = characterController.radius * 0.5f;
+        var origin = characterController.center - Vector3.up * (.5f * characterController.height - radius);
+        IsGrounded = Physics.SphereCast(
+            transform.TransformPoint(origin) + Vector3.up * characterController.contactOffset,
+            radius,
+            Vector3.down,
+            out var hit,
+            GroundedDistance + characterController.contactOffset,
+            GroundedLayerMask, QueryTriggerInteraction.Ignore);
+
+        GroundNormal = hit.normal;
+    }
+    protected virtual void HandleHMDMovement()
+    {
+        var originalCameraPosition = XRHead.transform.position;
+        var originalCameraRotation = XRHead.transform.rotation;
+
+        var delta = XRHead.transform.position - XRRig.transform.position;
+        delta.y = 0f;
+        if (delta.magnitude > 0.1f)
+        {
+            delta = Vector3.ProjectOnPlane(delta, GroundNormal);
+            XRRig.GetComponent<CharacterController>().Move(delta);
+        }
+
+        //transform.rotation = Quaternion.Euler(0.0f, XRHead.rotation.eulerAngles.y, 0.0f);
+
+        XRHead.transform.position -= delta;
     }
 }
