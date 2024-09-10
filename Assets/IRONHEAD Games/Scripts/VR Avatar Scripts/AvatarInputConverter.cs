e6f137f4723de8e7fc587b784613bef8909b09a1
Ken Cheng <24asasasas@gmail.com>
2024-09-10 19:05:12 +0800

    Update AvatarInputConverter.cs

--- Assets/IRONHEAD Games/Scripts/VR Avatar Scripts/AvatarInputConverter.cs
+++ Assets/IRONHEAD Games/Scripts/VR Avatar Scripts/AvatarInputConverter.cs
@@ -35,14 +35,12 @@ public class AvatarInputConverter : MonoBehaviour
         if(XRHead.TryGetComponent<CharacterController>(out CharacterController c))
         {
             MainAvatarTransform.position = Vector3.Lerp(MainAvatarTransform.position, XRHead.position + headPositionOffset + c.center + new Vector3(0, XRHead.GetComponent<CharacterController>().height / 2, 0), 0.5f);
+            AvatarHead.GetChild(0).rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.Find("CameraRig/FloorOffset/CameraScale/Camera").rotation, 0.5f);
+            AvatarHead.rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.rotation, 0.5f);
         }
         //Head and Body synch
         
-        AvatarHead.rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.rotation, 0.5f);
-        if(AvatarHead.GetChild(0) != null)
-        {
-            AvatarHead.GetChild(0).rotation = Quaternion.Lerp(AvatarHead.rotation, XRHead.Find("CameraRig/FloorOffset/CameraScale/Camera").rotation, 0.5f);
-        }
+        
         
         if (AvatarBody!=null)
         {
